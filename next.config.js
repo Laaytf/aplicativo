2aae7b35673cfa64748b060712f5c0e874df6519
Laaytf <Layslathauanne.21@gmail.com>
2026-01-14 20:03:56 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false,
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
-
   images: {
     remotePatterns: [
       {
